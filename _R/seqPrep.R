736960ebd5d0568217d345f36721c6e0dec181f1
Yao Li <nprotei@gmail.com>
2017-03-24 09:46:30 +0800

    to duplication removal

--- _R/seqPrep.R
+++ _R/seqPrep.R
@@ -1,14 +1,17 @@
-# Aim of the section: 
+# Aim of the project: 
 # to understand the subtype dist. in Gs/GD and 
 # inspect the essential cis-determinants
+# for this section:
+# 1. clean the sequence ID
+# 2. try to remove apparent duplicated sequences
 
 
-# clean ID and extract info ####
+# clean ID ####
 
 library(seqinr)
 library(stringr)
 
-  file = read.fasta(file.choose())
+  file = read.fasta("~/Desktop/ReassortSubtype/pool_ha_13193.fasta")
   
   seq_name0 = attributes(file)$names
        seq0 = getSequence(file)
@@ -44,18 +47,112 @@ library(stringr)
     gsub("--", "-07-01", seq_name[which( endsWith(seq_name, "--") == "TRUE")])
   
   # w/o day
-  seq_name[which( endsWith(seq_name, "-") == "TRUE")] <- 
-    gsub("-", "-15", seq_name[which( endsWith(seq_name, "-") == "TRUE")])
   
-
+  sub_seq <- which( endsWith(seq_name, "-") == "TRUE" )[ 
+    !which( endsWith(seq_name, "-") == "TRUE" ) %in% seq(12956, 13193) ]
+   
+  seq_name[sub_seq] <- paste0(seq_name[sub_seq], "15")
+    
+# deal with duplicated ID ####
   
-  # deal with duplicate ID / seq
+ # D (n = 3938)
   
   duplicated_ID <- which(duplicated(seq_name) == "TRUE")
-  duplicated_note = duplicated(seq_name)
   
+ # D2 (n = 4813)
   
+  n <- "A/([A-Za-z0-9-_]+)/([A-Za-z0-9-_]+)/([A-Za-z0-9-_]+)"
   
+  duplicated_ID2 <- which(duplicated(str_match(seq_name, n)[,1]) == TRUE)
   
+ # S (n = 5687)
+  
+ duplicated_seq <-  which(
+   
+   duplicated(sapply(seq0, function(x){
+     
+     y = c2s(x)
+     z = gsub("-", "", y)
+  
+   return(z) 
+     }  
+   )) == "TRUE")
 
+ 
+ # D2 & S (n = 4327)
+ 
+   duplicated_rm <- intersect(duplicated_ID2, duplicated_seq)
+ 
+ # to be edit: (n = 205)
+ 
+ duplicated_edit <- duplicated_ID[!duplicated_ID %in% duplicated_rm]
+  
+ 
+ # loop for each replicated case
+ 
+ if( length(duplicated_edit) > 0 ){
+   
+  # for all the duplicated id
+   
+   for (i in 1: length(duplicated_edit)){
+     
+     dup0 = which(match(seq_name, seq_name[duplicated_edit[i]]) != "NA")
+     
+     # but not in remove group
+     
+     dup0 = dup0[!dup0 %in% duplicated_rm]
+     
+     # create a null vector to appendex  
+     
+     app = c("", paste0(letters, "_"), toupper(paste0(letters, "_") ) )
+     
+     ap.id = seq_name[dup0]
+     app.id = c()
+     
+     # loop to deal with multiple replicated
+     # find the date info, insert labeling in the middle
+     
+     d = "([0-9]{4})-([0-9]{2})-([0-9]{2})"
+     
+     time_rep <- str_match(ap.id, d)[,1]
+     
+     for (k in 1: length(ap.id)){
+       
+       app.id[k] = sub(time_rep[k], paste0(app[k], time_rep[k]), ap.id[k])
+     }
+     
+     # back to seq_name0  
+     
+     seq_name[dup0] = app.id
+     
+   }
+ }     
+  
+ 
+# check and output ####
+ 
+ which(duplicated(seq_name[seq(1, length(seq_name))[-duplicated_rm]]) == TRUE)
+ 
 
+# sequence curation ####
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
+  
