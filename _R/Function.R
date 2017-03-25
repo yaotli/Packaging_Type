edfc1beb1a3891df99e17b497ecf2acb5623fdd5
Yao Li <nprotei@gmail.com>
2017-03-25 19:47:52 +0800

    fix bug in phylo_date; add cleanID, curateSeq

--- _R/Function.R
+++ _R/Function.R
@@ -18,6 +18,17 @@ phylo_date <- function(x){
                                        strptime(yr.0, format = "%Y-%m-%d"), 
                                        units = "days"))/365
   
+  # bug?
+  if ( is.na(daydifference) ){ 
+    
+    x <- sub(pattern = "01", replacement = "02", x)
+    daydifference <- as.numeric(difftime(strptime(x, format = "%Y-%m-%d"),
+                                         strptime(yr.0, format = "%Y-%m-%d"), 
+                                         units = "days"))/365
+    
+    
+  } 
+  
   yr.daydifference = yr + daydifference
   yr.daydifference <- format( round(yr.daydifference, 2), nsmall = 2)
   
@@ -27,3 +38,246 @@ return(yr.daydifference)
 
 
 
+# cleanID ####
+
+cleanID <- function(x){
+  
+  # require Function::phylo_date   
+  
+  library(seqinr)
+  library(stringr)
+  
+  # read-in
+  
+  file <- read.fasta(file.choose())
+  
+  seq_name0 = attributes(file)$names
+  seq0 = getSequence(file)
+  
+  # deal with ineligible id
+  
+  seq_name = gsub(" ", "_", seq_name0)
+  seq_name = gsub("\\(", "-", seq_name)
+  seq_name = gsub("\\)", "-", seq_name)
+  seq_name = gsub("\\[", "-", seq_name)
+  seq_name = gsub("\\]", "-", seq_name)
+  seq_name = gsub("\\'", "", seq_name)
+  seq_name = gsub("\\.", "-", seq_name)  
+  
+  seq_name = gsub(">", "", seq_name)  
+  
+  # for gisaid
+  seq_name = gsub("_A_/", "", seq_name)
+  
+  # deal with date
+  
+  # for gisaid
+  seq_name = gsub("_-Month_and_day_unknown-", "-07-01", seq_name)
+  seq_name = gsub("_-Day_unknown-", "-15", seq_name)
+  
+  # for ncbi
+  
+  # w/o all
+  seq_name[which( endsWith(seq_name, "_--") == "TRUE")] <- 
+    gsub("_--", "_9999-99-99", seq_name[which( endsWith(seq_name, "_--") == "TRUE")])
+  
+  # w/o month and day
+  seq_name[which( endsWith(seq_name, "--") == "TRUE")] <- 
+    gsub("--", "-07-01", seq_name[which( endsWith(seq_name, "--") == "TRUE")])
+  
+  # w/o day
+  sub_seq <- which( endsWith(seq_name, "-") == "TRUE" )
+  
+  seq_name[sub_seq] <- paste0(seq_name[sub_seq], "15")
+  
+  
+  # deal with duplicated ID  
+  
+  duplicated_id = which(duplicated(seq_name) == "TRUE")
+  
+  # loop for adding pseudocode
+  
+  if( length(duplicated_id) > 0 ){
+    
+    # for all the duplicated id
+    
+    for (i in 1: length(duplicated_id)){
+      
+      dup0 = which( match(seq_name, seq_name[duplicated_id[i]] ) != "NA" )
+      
+      # create a null vector to appendex  
+      
+      app = c("", paste0(letters, "_"), toupper(paste0(letters, "_") ) )
+      
+      ap.id = seq_name[dup0]
+      app.id = c()
+      
+      # loop to deal with multiple replicated
+      # find the date info, insert labeling in the middle
+      
+      d = "([0-9]{4})-([0-9]{2})-([0-9]{2})"
+      
+      time_rep <- str_match(ap.id, d)[,1]
+      
+      for (k in 1: length(ap.id)){
+        
+        app.id[k] = sub(time_rep[k], paste0(app[k], time_rep[k]), ap.id[k])
+      }
+      
+      # back to seq_name
+      
+      seq_name[dup0] = app.id
+      
+    }
+  }     
+  
+  duplicated_id_ed = which(duplicated(seq_name) == "TRUE")
+  
+  if (length(duplicated_id_ed) > 0 ){
+    
+    print("ERROR") 
+    
+  }else{
+    
+    # generate temporal info
+    
+    d = "([0-9]{4})-([0-9]{2})-([0-9]{2})"
+    
+    for (i in 1: ( length(seq0)) ) {
+      
+      seq_name[i] <- gsub(x = seq_name[i], pattern = d, replacement = 
+                            
+                            phylo_date( str_match(string = seq_name[i], pattern = d)[,1] ) )
+      
+    }
+    
+    write.fasta(seq0, 
+                file.out = "~/Desktop/cleanID.fasta", 
+                names = seq_name)
+    
+    print("DONE")
+    
+  }
+  
+  
+}
+
+
+
+# curateSeq ####
+
+curateSeq <- function(maxamb = 5, minseq = 1600, mode = 1, vip = 0){
+  
+  # should apply after cleanID
+  # mode 1 : curation; 2 : duplicated seq; 3 : duplicated id; 4 : similar id
+  #      5 : 1x2x3 ;   6 : 1x2x4
+  #      7 : 1+2+3 ;   8 : 1+2+4    
+  
+  
+  
+  library(seqinr)
+  library(stringr)
+  
+  # read-in
+  
+  file = read.fasta(file.choose())
+  
+  seq_name0 = attributes(file)$names
+  seq0 = getSequence(file)
+  
+  # sequence curation [1]
+  
+  # for ambiguous nucleotide and seq length
+  
+  tobedelect1 <- c()
+  
+  for(i in 1: length(seq0)){
+    
+    ATCG = c("a", "t", "c", "g")
+    
+    # convert character to string
+    
+    seq_0 = c2s( seq0[[i]] )
+    seq_i = gsub("-", "", seq_0)
+    seq_i = gsub("~", "", seq_i)
+    
+    # seq length and number of ambiguous nucleotide
+    
+    seqlth = length( s2c(seq_i) )
+    amb = length( which(! s2c(seq_i) %in% ATCG ) )
+    
+    if( ( seqlth < minseq ) | ( amb > maxamb ) ){
+      
+      tobedelect1[length(tobedelect1) + 1 ] = i
+      
+    }
+    
+  }
+  
+  print( paste0("curate: ", length(tobedelect1) ) )
+  
+  # duplicated sequcnes [2]
+  
+  dup_seq <-  which(
+    
+    duplicated( sapply(seq0, function(x){
+      
+      y = c2s(x)
+      z = gsub("-", "", y)
+      
+      return(z) 
+    }  
+    )) == "TRUE")
+  
+  print( paste0("dupSeq: ", length(dup_seq) ) )
+  
+  # duplicated id (after cleanID) [3]
+  
+  du = "_([HN0-9]+)_([a-zA-z])_"
+  dup_id <- grep(du, seq_name0)
+  
+  print( paste0("dupID: ", length(dup_id) ) )
+  
+  # similar id [4]
+  
+  m = "a/([A-Za-z0-9-_]+)/([A-Za-z0-9-_]+)/([A-Za-z0-9-_]+)"
+  
+  sim_id <- which( duplicated(str_match( tolower(seq_name0), m)[,1]) == TRUE)
+  
+  print( paste0("simID: ", length(sim_id) ) )
+  
+  # mode 
+  
+  mode1x2 <- sort( unique( c(tobedelect1, dup_seq) ) )
+  mode1x3 <- sort( unique( c(tobedelect1, dup_id ) ) )
+  mode1x4 <- sort( unique( c(tobedelect1, sim_id ) ) )
+  
+  mode1x2x3 <- sort( unique( c(tobedelect1, intersect(dup_seq, dup_id)) ) )
+  mode1x2x4 <- sort( unique( c(tobedelect1, intersect(dup_seq, sim_id)) ) )
+  
+  mode1p2p3 <- sort( unique( c(tobedelect1, dup_seq, dup_id ) ) )
+  mode1p2p4 <- sort( unique( c(tobedelect1, dup_seq, sim_id ) ) )
+  
+  
+  tobedelect_f <- list(tobedelect1, mode1x2, mode1x3, mode1x4, 
+                       mode1x2x3, mode1x2x4, 
+                       mode1p2p3, mode1p2p4)
+  
+  remain <- seq(1: length(seq0))[- tobedelect_f[[mode]] ]
+  
+  # special list
+  
+  remain <- sort( unique( c( remain, rev(seq(1: length(seq0) ))[1:vip] ) ) )
+  
+  seq_name_out <- seq_name0[remain]
+  seq_out <- seq0[remain]
+  
+  write.fasta(seq_out,
+              file.out = paste0("~/Desktop/curateSeq", "-", mode, ".fasta"), 
+              names = seq_name_out)
+  
+  print( paste0("delete: ", length(tobedelect_f[[mode]]) ) )
+  print( paste0("remain: ", length(remain) ) )
+  
+  
+}
