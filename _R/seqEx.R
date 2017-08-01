61f920edd5ba178c80469b55727604f7141fb9a7
Yao Li <nprotei@gmail.com>
2017-08-01 16:10:28 +0800

    rearrange with ntSub_time

--- _R/seqEx.R
+++ _R/seqEx.R
@@ -1,10 +1,122 @@
+# data preparation following seqPrep.R
+
+library(seqinr)
+library(stringr)
+library(ggtree)
+library(ape)
+library(dplyr)
 
 setwd("~/Desktop/data_souce/")
 source("~/Packaging_Type/_R/Function.R")
 
-pool_table    <- read.csv("./pool_df.csv", header = TRUE, stringsAsFactors = FALSE)
-h5_GsGD_table <- read.csv("./h5_GsGD_table.csv", header = TRUE, stringsAsFactors = FALSE)
-n1_table      <- read.csv("./n1_table.csv", header = TRUE, stringsAsFactors = FALSE)
+pool_csv          <- "./pool_df.csv"
+gsgdtree          <- "./Tree/h5_GsGD"
+n1tree            <- "./Tree/N1_pool"
+
+# result files
+h5_GsGD_table_dir <- "./h5_GsGD_table.csv"
+n1_table_dir      <- "./n1_table.csv"   
+
+### Clade info ----------------------------------
+
+## HA ----------------
+
+# tree file import and data retrive 
+
+pool_table      <- read.csv(pool_csv, header = TRUE, stringsAsFactors = FALSE)
+
+h5_GsGDfile     <- read.tree(gsgdtree)
+h5_GsGDtreedata <- fortify(h5_GsGDfile)
+
+rootnode        <- length(h5_GsGDfile$tip.label) + 1
+
+# info for the dataframe
+
+h5_GsGD_R2tip <- dist.nodes(h5_GsGDfile)[rootnode, 1: (rootnode - 1)]
+
+h5_GsGD_name  <- gsub("'", "", h5_GsGDtreedata$label[1: rootnode - 1] )
+h5_GsGD_time  <- as.numeric( str_match( h5_GsGD_name, "[0-9]{4}\\.[0-9]{2,3}") )
+h5_GsGD_NA    <- str_match( h5_GsGD_name, "(_H5)(N[0-9])")[,3]
+
+ac_code       <- "EPI[0-9]+|[A-Z]{1,2}[0-9]{5,6}"
+h5_GsGD_ac    <- str_match( h5_GsGD_name, ac_code )
+
+h5_GsGD_geo   <- pool_table$geo[ match(h5_GsGD_ac, pool_table$ac) ] 
+
+h5_GsGD_table <- data.frame(name    = h5_GsGD_name,  
+                            subtype = h5_GsGD_NA,
+                            ac      = h5_GsGD_ac,
+                            geo     = h5_GsGD_geo,
+                            distance = h5_GsGD_R2tip, 
+                            time     = h5_GsGD_time, 
+                            stringsAsFactors = FALSE)
+
+
+# clade info
+# manually prepare txt file for clade annotation
+
+for(i in 1: length( list.files("./Clade/clade_txt/") ) )
+{
+  cladename_i   <- read.table(file = 
+                                paste0("./Clade/clade_txt/", list.files("./Clade/clade_txt/")[i]),
+                              stringsAsFactors = FALSE)
+  
+  cladematch    <- as.numeric( h5_GsGD_name %in% cladename_i[,1] )
+  
+  h5_GsGD_table[, ncol(h5_GsGD_table) + 1] =  cladematch
+  
+  colnames( h5_GsGD_table )[ ncol( h5_GsGD_table ) ] =
+    sub(".txt", "", list.files("./Clade/clade_txt/")[i] )
+}
+
+
+## NA ----------------
+
+n1_file     <- read.tree(n1tree)
+n1_treedata <- fortify(n1_file)
+
+rootnode_nu <- length(n1_file$tip.label) + 1
+
+# info for the dataframe
+
+n1_R2tip <- dist.nodes(n1_file)[rootnode_nu, 1: (rootnode_nu - 1)]
+
+n1_name  <- gsub("'", "", n1_treedata$label[1: rootnode_nu - 1] )
+n1_time  <- as.numeric( str_match( n1_name, "[0-9]{4}\\.[0-9]{2,3}") )
+
+ac_code  <- "EPI[0-9]+|[A-Z]{1,2}[0-9]{5,6}"
+n1_ac    <- str_match( n1_name, ac_code)
+
+n1_geo   <- pool_table$geo[ match(n1_ac, pool_table$ac) ] 
+
+n1_table <- data.frame( name     = n1_name,  
+                        ac       = n1_ac,
+                        geo      = n1_geo,
+                        distance = n1_R2tip, 
+                        time     = n1_time, 
+                        stringsAsFactors = FALSE)
+
+## match NA to HA ----------------
+
+for(k in 7: ncol(h5_GsGD_table) )
+{
+  
+  n1_table_i <- 
+    h5_GsGD_table[, k][ match( pool_table$pac[ match( n1_table$ac, pool_table$ac ) ], 
+                               h5_GsGD_table$ac ) ]
+  
+  n1_table_i[ is.na(n1_table_i) ] = 0
+  
+  n1_table[, ncol(n1_table) + 1] = n1_table_i
+  colnames( n1_table )[ ncol(n1_table) ] = colnames(h5_GsGD_table)[k]
+  
+}
+
+
+# export 
+# write.csv(h5_GsGD_table, "h5_GsGD_table.csv")
+# write.csv(n1_table, "n1_table.csv")
+
 
 
 ### HA and N1 from 3 clades in China --------------------------------
@@ -14,7 +126,7 @@ n1_table      <- read.csv("./n1_table.csv", header = TRUE, stringsAsFactors = FA
 
 # HA 
 
-for(i in 8: 10)
+for(i in 7: 9)
 {
   index = which( ( h5_GsGD_table$geo == "China" | h5_GsGD_table$geo == "Hong_Kong" ) &  
                    h5_GsGD_table$subtype == "N1" & h5_GsGD_table[, i] == 1 )
@@ -22,33 +134,136 @@ for(i in 8: 10)
   subfastaSeq(filedir = "./align_trim/trim_h5_pool_lth1683.fasta", 
               AC      = TRUE,
               AC_list = h5_GsGD_table$ac[ index ], 
-              no      = colnames(h5_GsGD_table)[i] )
+              no      = colnames(h5_GsGD_table)[i])
 }
 
   
-
 # NA
 
-for(i in 7: 9)
+for(i in 6: 8)
 {
   index = which( ( n1_table$geo == "China" | n1_table$geo == "Hong_Kong" ) & 
                        n1_table[, i] == 1 )
   
-  subfastaSeq(filedir = "./align_trim/trim_nu_pool_H5N1-1000-3000_lth1350.fasta", 
+  subfastaSeq(filedir = "./align_trim/trim_nu_pool_H5N1_lth1347.fasta", 
               AC      = TRUE,
               AC_list = n1_table$ac[ index ], 
               no      = colnames(n1_table)[i] )
 }
 
 
+# fasttree for 2 genes each clade
+#
+# cd ~/Desktop/data_souce/Clade/clade_CNHK_fas/
+# for f in $(ls *.fasta);
+# do ~/fasttree -nt -spr 4 -nni -gtr -cat 20 -gamma -notop <$f> ~/Desktop/data_souce/Clade/sampled_clade_tree_CNHK/$f.tre ;
+# done
 
-# clade tree (geo: CN + HK )
-# fasttree (shell: fastttree)
 
-### Sampled and filtered seq --------------------------------
+### updated clade assignment by tree files --------------------------------
+
+# read-in
+# h5_GsGD_table <- read.csv( h5_GsGD_table_dir, header = TRUE, stringsAsFactors = FALSE)
+# n1_table      <- read.csv( n1_table_dir, header = TRUE, stringsAsFactors = FALSE)
+
+
+dir_sampledtre  <- "./Clade/sampled_clade_tree_CNHK/sampled_tre/"
+list_sampledtre <- list.files( dir_sampledtre )
+
+for( k in 1: length(list_sampledtre) )
+{
+  tre_k <- read.csv( paste0(dir_sampledtre, list_sampledtre[k]) , stringsAsFactors = FALSE)[, 1]
+  
+  ntax  <- as.numeric( str_match(tre_k [ grep("ntax", tre_k) ], "(ntax=)([0-9]+)")[,3] )
+  tax_s <- grep("ntax", tre_k) + 2
+  tax_e <- tax_s + ntax - 1
+  
+  taxaname <- tre_k[tax_s: tax_e]
+  reded    <- grep("#ff0000", taxaname)
+  remain   <- taxaname[ - reded ]
+  remain   <- gsub("\t|\\'", "", remain)
+  remain   <- gsub("\\[\\&\\!color\\=#[A-Za-z0-9]{6}\\]", "", remain)
+  
+  write.table(remain, 
+              paste0("./Clade/sampled_clade_tree_CNHK/sampled_txt/", 
+                     gsub("\\.|fasta|tre", "", list_sampledtre[ k ]) ), 
+              col.names = F, row.names = F, quote = F)
+  
+  print( length(remain) )
+  
+}
+
+
+# HA
+
+for(i in 1: length( grep("_h5", list.files("./Clade/updated/") ) ) )
+{
+  
+  k = grep("_h5", list.files("./Clade/updated/") )[ i ]
+  
+  cladename_i   <- read.table( file = 
+                                 paste0("./Clade/updated/", list.files("./Clade/updated/")[ k ]),
+                               stringsAsFactors = FALSE)
+  
+  cladematch    <- as.numeric( h5_GsGD_table$name %in% cladename_i[,1] )
+  
+  updatname     <- list.files("./Clade/updated/")[ k ]
+  
+  if( updatname %in% colnames(h5_GsGD_table) == TRUE )
+  {
+    j = which( colnames(h5_GsGD_table) %in% updatname == TRUE )
+    
+    h5_GsGD_table[ ,j] = cladematch
+    
+  }else
+  {
+    h5_GsGD_table[, ncol(h5_GsGD_table) + 1] =  cladematch
+    
+    colnames( h5_GsGD_table )[ ncol( h5_GsGD_table ) ] = updatname
+    }
+  
+}
+
+# NA 
+
+for(i in 1: length( grep("_n1", list.files("./Clade/updated/") ) ) )
+{
+  
+  k = grep("_n1", list.files("./Clade/updated/") )[ i ]
+  
+  cladename_i   <- read.table( file = 
+                                 paste0("./Clade/updated/", list.files("./Clade/updated/")[ k ]),
+                               stringsAsFactors = FALSE)
+  
+  cladematch    <- as.numeric( n1_table$name %in% cladename_i[,1] )
+  
+  updatname     <- list.files("./Clade/updated/")[ k ]
+  
+  if( updatname %in% colnames(n1_table) == TRUE )
+  {
+    j = which( colnames(n1_table) %in% updatname == TRUE )
+    
+    n1_table[ ,j] = cladematch
+    
+  }else
+  {
+    n1_table[, ncol(n1_table) + 1] =  cladematch
+    
+    colnames( n1_table )[ ncol( n1_table ) ] = updatname
+  }
+  
+}
+
+
+## Export table --------------------------------
+
+write.csv(h5_GsGD_table, "h5_GsGD_table.csv")
+write.csv(n1_table, "n1_table.csv")
+
+### Extract sampled and filtered seq --------------------------------
 
 # HA 
-for(i in 12: ncol(h5_GsGD_table) )
+for(i in 11: ncol(h5_GsGD_table) )
 {
   index = which(h5_GsGD_table[, i] == 1 )
   
@@ -61,16 +276,23 @@ for(i in 12: ncol(h5_GsGD_table) )
 
 
 # NA 
-for(i in 11: ncol(n1_table) )
+for(i in 10: ncol(n1_table) )
 {
   index = which(n1_table[, i] == 1 )
   
-  subfastaSeq(filedir = "./align_trim/trim_nu_pool_H5N1-1000-3000_lth1350.fasta", 
+  subfastaSeq(filedir = "./align_trim/trim_nu_pool_H5N1_lth1347.fasta", 
               AC      = TRUE,
               AC_list = n1_table$ac[ index ], 
               no      = colnames(n1_table)[i] )
   
 }
+
+# batch rename 
+#
+# cd cd ~/Desktop/data_souce/Clade/sampled_clade_tree_CNHK/sampled_fas/
+# for f in *.fasta; do mv "$f" "${f/ac/}"; done
+# for f in *.fasta; do mv "$f" "${f/_trim_h5_pool_lth1683/}"; done
+# for f in *.fasta; do mv "$f" "${f/_trim_nu_pool_H5N1_lth1347/}"; done
 
 ### Sampled seq of clade 232 before 2014 --------------------------------
 
