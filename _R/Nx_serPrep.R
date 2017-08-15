b6a099e3eacf5ceeefb3aaa982b5a826f7f4d42a
Yao Li <nprotei@gmail.com>
2017-08-15 14:47:59 +0800

    revise data source

--- _R/Nx_serPrep.R
+++ _R/Nx_serPrep.R
@@ -6,16 +6,49 @@ library(ape)
 setwd("~/Desktop/data_souce/")
 source("~/Packaging_Type/_R/Function.R")
 
-h5selected <- "./Nx/h5n5.txt"
-HAseqfile  <- "./align_trim/trim_h5_pool_lth1683.fasta"
+h5_raw_ncbi <- "./raw/H5_N_7412_20170815.fasta"
+
+### seq name cleaning --------------------------------
+
+# readin
+ha_n_seq    <- fastaEx(h5_raw_ncbi)$seq
+ha_n_id     <- fastaEx(h5_raw_ncbi)$id
+
+# deal with _A/
+ha_n_id[ grep( "_A/", ha_n_id, invert = TRUE) ] <-
+  str_replace( string      = ha_n_id[grep( "_A/", ha_n_id, invert = TRUE) ], 
+               pattern     = "([A-Z]{1,2})([0-9]{5,6})_", 
+               replacement = paste0( 
+                 str_match(ha_n_id[grep( "_A/", ha_n_id, invert = TRUE) ], 
+                           "([A-Z]{1,2})([0-9]{5,6})_")[,1], "A/") )
+
+ha_n_id <- gsub("A/", "", ha_n_id)
+
+# export
+write.fasta(ha_n_seq, ha_n_id, "h5_n_nonpaired.fasta")
+
+# cleanID
+cleanID("./Nx/h5_n_nonpaired.fasta")
+
+# curate
+# delete = 571 | remain = 6841
+curateSeq(maxamb = 150, minseq = 1500, mode = 1, filedir = "./Nx/cleanID_h5_n_nonpaired.fasta")
+
+# align the seq by MAFFT
+# SHELL:
+# 
+# cd ~/Desktop/data_souce/Nx
+# mafft ./curateSeq-1_cleanID_h5_pool.fasta > ./align_trim/align_h5_pool.fasta
+#
+# manually trim in BioEdit (remove stop codon & ~, lth = 1683)
+
 
 ## extract sequences ----------------
 
-subtreseq(seq_filedir = HAseqfile, list_filedir = h5selected)
 
 # check by raxml tree
-# ./raxml_AVX2 -f a -p 666 -s s_h5_n5_2344.fasta -x 616 -#autoMRE -m GTRGAMMAI --HKY85 -n h5n5
+# ./raxml_AVX2 -f a -p 666 -s h5_n5.fasta.fasta -x 616 -#autoMRE -m GTRGAMMAI --HKY85 -n h5n5
 # 
 # check substitution model
-# java -jar /Volumes/EDGE\ 2/Apps/jmodeltest-2.1.10/jModelTest.jar -d /Users/yaosmacbook/Desktop/data_souce/Nx/s_h5_n5_2344.fas -f -i -g 4 -s 5 -BIC -a -S NNI
+# java -jar /Volumes/EDGE\ 2/Apps/jmodeltest-2.1.10/jModelTest.jar -d /Users/yaosmacbook/Desktop/data_souce/Nx/h5_n5_phy -f -i -g 4 -s 5 -BIC -a -S SPR
 
